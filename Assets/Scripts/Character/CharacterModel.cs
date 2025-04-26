1e7b989f70be5ab6edb11473327dbe91eec160ef
RyooTaeHeon <taroni55@naver.com>
2025-04-26 16:53:35 +0900

    [Feat] 캐릭터 Model 구현

---
+++ Assets/Scripts/Character/CharacterModel.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class CharacterModel : MonoBehaviour
+{
+    public float moveSpeed = 1f;
+
+    public float attackRange = 2f;
+
+    public float attackDamage = 10f;
+
+    public float attackInterval = 1f;
+
+    public LayerMask enemyLayer;
+}
