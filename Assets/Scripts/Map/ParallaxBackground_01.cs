11e158af184689765753bf3fa6944250bb3f7223
RyooTaeHeon <taroni55@naver.com>
2025-04-26 16:09:13 +0900

    [Feat] 2D 맵 스크롤 기능 구현

---
+++ Assets/Scripts/Map/ParallaxBackground_01.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour
+{
+    [SerializeField] Transform _target;            // 현재 배경과 이어지는 배경
+
+    [SerializeField] float _scrollAmount;          // 이어지는 두 배경 사이 거리
+
+    [SerializeField] float _moveSpeed;             // 이동 속도
+
+    [SerializeField] Vector3 _moveDirection;       // 이동 방향
+
+    private void Update()
+    {
+        // 배경이 _moveDirection 방향으로 _moveSpeed 속도로 이동
+        transform.position += _moveDirection * _moveSpeed * Time.deltaTime;
+
+        // 배경이 설정된 범위를 벗어나면 위치 재설정
+        if (transform.position.x <= -_scrollAmount)
+        {
+            // 이동 방향의 반대 방향 * _scrollAmount만큼 간격으로 위치 재설정
+            transform.position = _target.position - _moveDirection * _scrollAmount;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Map/ParallaxBackground_02.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class ParallaxBackground_02 : MonoBehaviour
+{
+    [SerializeField] float[] layerMoveSpeed;         // z 값이 다른 배경 레이어 별 이동 속도
+
+    private int backgroundCount;                     // Layer 수
+
+    private Material[] materials;                    // 배경 스크롤을 위한 Material 배열 변수
+
+    private void Awake()
+    {
+        // 배경의 개수를 구하고, 배경 정보를 저장할 GameObject 배열 선언
+        backgroundCount = transform.childCount;
+        GameObject[] backgrounds = new GameObject[backgroundCount];
+
+        // 각 배경의 material과 이동 속도를 저장할 배열 선언
+        materials = new Material[backgroundCount];
+        layerMoveSpeed = new float[backgroundCount];
+
+
+        for (int i = 0; i < backgroundCount; ++i)
+        {
+            backgrounds[i] = transform.GetChild(i).gameObject;
+            materials[i] = backgrounds[i].GetComponent<Renderer>().material;
+        }
+
+        SetLayerMoveSpeed();
+    }
+
+    /// <summary>
+    /// 각 Layer의 moveSpeed 설정
+    /// </summary>
+    private void SetLayerMoveSpeed()
+    {
+        float stackSpeed = 0.01f;
+        for (int i = 1; i < backgroundCount; i++)
+        {
+            layerMoveSpeed[i] = stackSpeed;
+            stackSpeed += 0.01f;
+        }
+    }
+
+    /// <summary>
+    /// 각 Layer 스크롤
+    /// </summary>
+    private void Update()
+    {
+        for (int i = 1; i < materials.Length; ++i)
+        {
+            materials[i].SetTextureOffset("_MainTex", Vector2.right * layerMoveSpeed[i] * Time.time);
+        }
+    }
+}
