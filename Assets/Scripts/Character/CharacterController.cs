f48b551dd50310e19ff8b049424ac46fbdd780aa
RyooTaeHeon <taroni55@naver.com>
2025-04-26 16:53:17 +0900

    [Feat] 캐릭터 FSM 구현

---
+++ Assets/Scripts/Character/CharacterController.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using UnityEngine;
+using DG.Tweening;
+
+public enum CharacterState { Idle, Move, Detect, Attack }
+
+public class CharacterController : MonoBehaviour, IDamageble
+{
+    public CharacterModel _model;
+
+    private Transform monster;
+
+    private CharacterState currentState = CharacterState.Idle;
+
+    void Update()
+    {
+        switch (currentState)
+        {
+            case CharacterState.Idle:
+                Move();
+                SearchForEnemies();
+                break;
+
+            case CharacterState.Move:
+                Move();
+                SearchForEnemies();
+                break;
+
+            case CharacterState.Detect:
+                if (monster != null)
+                    StartCoroutine(AttackEnemy());
+                break;
+        }
+    }
+
+    void SearchForEnemies()
+    {
+        Collider2D enemy = Physics2D.OverlapCircle(transform.position, _model.attackRange, _model.enemyLayer);
+        if (enemy != null)
+        {
+            monster = enemy.transform;
+            currentState = CharacterState.Detect;
+        }
+    }
+
+    void Move()
+    {
+        // 단순 이동 로직 (예: 플레이어 입력, AI 이동 가능)
+        transform.Translate(Vector2.right * _model.moveSpeed * Time.deltaTime);
+    }
+
+    IEnumerator AttackEnemy()
+    {
+        currentState = CharacterState.Attack;
+
+        while (monster != null)
+        {
+            monster.GetComponent<IDamageble>().TakeDamage(_model.attackDamage);
+            monster.DOShakePosition(0.3f, 0.2f);
+
+            yield return new WaitForSeconds(_model.attackInterval);
+
+            SearchForEnemies(); // 공격 후 다시 적 탐색
+        }
+
+        currentState = CharacterState.Idle;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, _model.attackRange); // 공격 범위 표시
+    }
+
+    public void TakeDamage(float Damage)
+    {
+        throw new System.NotImplementedException();
+    }
+}
