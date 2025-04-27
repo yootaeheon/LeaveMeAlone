387f84f7f268eb16b65aaef0d84f4b27fcdf3618
RyooTaeHeon <taroni55@naver.com>
2025-04-27 14:31:10 +0900

    [Feat] 카메라 Util 클래스 생성

---
+++ Assets/Scripts/CameraUtil.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public static class CameraUtil
+{
+    /// <summary>
+    /// 카메라 어두워졌다 밝아지는 FadeIn 기능
+    /// </summary>
+    public static void CameraFadeIn()
+    {
+        Camera.main.GetComponent<Animator>().SetTrigger("FadeIn");
+    }
+
+
+    /// <summary>
+    /// 카메라 어두워지는 FadeOut 기능
+    /// </summary>
+    public static void CameraFadeOut()
+    {
+        Camera.main.GetComponent<Animator>().SetTrigger("FadeOut");
+
+    }
+}
