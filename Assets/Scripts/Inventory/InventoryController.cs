898c1d6ddb856b6b668eaedc6e9d57b7f367fc81
RyooTaeHeon <taroni55@naver.com>
2025-05-18 12:20:31 +0900

    [Feat] 스크립트 주석 추가

--- Assets/Scripts/Inventory/InventoryController.cs
+++ Assets/Scripts/Inventory/InventoryController.cs
@@ -7,22 +7,31 @@ using Inventory.View;
 namespace Inventory
 {
     /// <summary>
-    /// InventoryPage와 InventorySO를 중개해줌;
+    /// InventoryPage(UI)와 InventorySO(데이터 모델)를 중개하는 컨트롤러 역할
     /// </summary>
     public class InventoryController : MonoBehaviour
     {
-        [SerializeField] UIInventoryPage _inventoryUI;
-
-        [SerializeField] InventorySO _inventoryData;
+        [SerializeField] UIInventoryPage _inventoryUI;       // UI
+                                                          
+        [SerializeField] InventorySO _inventoryData;         // Data
 
         private void Start()
         {
             PrepareUI();
             /* _inventoryData.Init();*/
         }
+        private void PrepareUI()
+        {
+            _inventoryUI.InityInventoryUI(_inventoryData.Size);               // UI 슬롯 개수 초기화
+
+            _inventoryUI.OnDescriptionRequested += RequestDescription;  // 설명 요청 이벤트 구독
+            _inventoryUI.OnSwapItems += HandleSwapItems;                      // 아이템 교환 이벤트 구독
+            _inventoryUI.OnStartDragging += HandleDragging;                   // 드래그 시작 시 이벤트 구독
+            _inventoryUI.OnItemActionRequested += HandleItemActionRequest;    // 아이템 액션 요청 이벤트 구독
+        }
 
         /// <summary>
-        /// 인벤토리 열기 메서드
+        /// 인벤토리 열기/닫기 메서드
         /// 인벤토리 버튼에 연결
         /// </summary>
         public void OnInventory()
@@ -41,17 +50,16 @@ namespace Inventory
             }
         }
 
-        private void PrepareUI()
-        {
-            _inventoryUI.InityInventoryUI(_inventoryData.Size);
-            _inventoryUI.OnDescriptionRequested += HandleDescriptionRequest;
-            _inventoryUI.OnSwapItems += HandleSwapItems;
-            _inventoryUI.OnStartDragging += HandleDragging;
-            _inventoryUI.OnItemActionRequested += HandleItemActionRequest;
-        }
+       
 
         #region InventoryUI에 문의하여 모델 데이터 접근
-        private void HandleDescriptionRequest(int itemIndex)
+        /// <summary>
+        /// 아이템 설명 요청 처리
+        /// 해당 인덱스 아이템 데이터 가져와서 UI에 설명 업데이트
+        /// 아이템이 비었으면 선택 초기화
+        /// </summary>
+        /// <param name="itemIndex"></param>
+        private void RequestDescription(int itemIndex)
         {
             InventoryItem inventoryItem = _inventoryData.GetItemIndex(itemIndex);
             if (inventoryItem.IsEmpty)
@@ -63,11 +71,22 @@ namespace Inventory
             _inventoryUI.UpdateDescription(itemIndex, item.ItemImage, item.name, item.Description);
         }
 
+        /// <summary>
+        /// 아이템 위치 교환 처리
+        /// InventorySO(데이터 모델)에 아이템 스왑 메서드 호출
+        /// </summary>
+        /// <param name="itemIndex_1"></param>
+        /// <param name="itemIndex_2"></param>
         private void HandleSwapItems(int itemIndex_1, int itemIndex_2)
         {
             _inventoryData.SwapItems(itemIndex_1, itemIndex_2);
         }
 
+        /// <summary>
+        /// 드래그 시작 시 처리
+        /// 드래그하는 아이템의 이미지와 수량 정보를 MouserFollwer에 똑같이 생성
+        /// </summary>
+        /// <param name="itemIndex"></param>
         private void HandleDragging(int itemIndex)
         {
             InventoryItem inventoryItem = _inventoryData.GetItemIndex(itemIndex);
@@ -76,6 +95,14 @@ namespace Inventory
             _inventoryUI.CreateDraggedItem(inventoryItem.Item.ItemImage, inventoryItem.Quantity);
         }
 
+        /// <summary>
+        /// 아이템 사용 등 액션 요청 처리
+        /// 아이템이 비었으면 선택해제
+        /// 
+        /// 그렇지 않으면 아이템 설명 업데이트 및 아이템 액션 호출
+        /// 필요시 아이템 소모 처리
+        /// </summary>
+        /// <param name="itemIndex"></param>
         private void HandleItemActionRequest(int itemIndex)
         {
             InventoryItem inventoryItem = _inventoryData.GetItemIndex(itemIndex);
@@ -86,19 +113,7 @@ namespace Inventory
 
             ItemSO item = inventoryItem.Item;
             _inventoryUI.UpdateDescription(itemIndex, item.ItemImage, item.name, item.Description);
-
-             IItemAction itemAction = inventoryItem.Item as IItemAction;
-            if (itemAction != null)
-            {
-                itemAction.Use();
-            }
-            IDestoryableItem destroyableItem = inventoryItem.Item as IDestoryableItem;
-            if (destroyableItem != null)
-            {
-                _inventoryData.RemoveItem(itemIndex, 1);
-            }
         }
         #endregion
-
     }
 }
--- Assets/Scripts/UI/UIInventoryPage.cs
+++ Assets/Scripts/UI/UIInventoryPage.cs
@@ -6,6 +6,17 @@ using UnityEngine;
 
 namespace Inventory.View
 {
+    // UI → InventoryController → InventorySO → UI 의 흐름으로 움직이며,
+    // 이벤트를 통해 UI와 데이터 모델 간 데이터 요청, 변경, 갱신이 이루어집니다.
+
+    /// <summary>
+    /// 인벤토리 UI 한 페이지 전체 관리 클래스
+    /// 1. 아이템 UI 슬롯 생성
+    /// 2. 선택
+    /// 3. 드래그 앤 드롭
+    /// 4. 설명 표시
+    /// 5. 액션 요청
+    /// </summary>
     public class UIInventoryPage : MonoBehaviour
     {
         [SerializeField] UIInventoryItem _itemPrefab;
@@ -16,16 +27,19 @@ namespace Inventory.View
 
         [SerializeField] MouseFollower _mouseFollower;
 
-        List<UIInventoryItem> listOfUIItems = new List<UIInventoryItem>();
+        List<UIInventoryItem> listOfUIItems = new List<UIInventoryItem>();    // 현재 페이지 내 아이템 UI 슬로 리스트
 
-        private int curDraggedIndex = -1;
+        private int curDraggedIndex = -1; // 현재 드래그 중인 아이템 인덱스
 
+        /// <summary>
+        /// 외부에서 구독 하는 이벤트
+        /// </summary>
         public event Action<int>
-            OnDescriptionRequested,
-            OnItemActionRequested,
-            OnStartDragging;
+            OnDescriptionRequested,    // 어아탬 설명 요청
+            OnItemActionRequested,     // 아이템 액션 요청 시 (우클릭 메뉴) // 우클릭 기능 삭제 예정
+            OnStartDragging;           // 드래그 시작 시
 
-        public event Action<int, int> OnSwapItems;
+        public event Action<int, int> OnSwapItems; // 아이템 위치 변경 요청 시(드래그-드롭)
 
 
         private void Awake()
@@ -35,6 +49,10 @@ namespace Inventory.View
             _itemDescription.ResetDescription();
         }
 
+        /// <summary>
+        /// 인벤토리 UI 슬롯 초기화 및 생성
+        /// </summary>
+        /// <param name="inventorySize"></param>
         public void InityInventoryUI(int inventorySize)
         {
             for (int i = 0; i < inventorySize; i++)
@@ -44,16 +62,16 @@ namespace Inventory.View
                 uiItem.transform.localScale = Vector2.one;
                 listOfUIItems.Add(uiItem);
 
-                uiItem.OnItemClicked += HandleItemSelection;
-                uiItem.OnItemBeginDrag += HandleBegingDrag;
-                uiItem.OnItemDroppedOn += HandleSwap;
-                uiItem.OnItemEndDrag += HandleEndDrag;
-                uiItem.OnRightMouseButtonClick += HandleShowItemActions;
+                uiItem.OnItemClicked += SelectItem;
+                uiItem.OnItemBeginDrag += BegingDrag;
+                uiItem.OnItemDroppedOn += Swap;
+                uiItem.OnItemEndDrag += EndDrag;
+                uiItem.OnRightMouseButtonClick += ShowItemActions;
             }
         }
 
         /// <summary>
-        /// 
+        /// 인벤토리 UI 활성화 및 선택 초기화
         /// </summary>
         public void Show()
         {
@@ -61,18 +79,27 @@ namespace Inventory.View
             ResetSelection();
         }
 
+        /// <summary>
+        /// 인벤토리 UI 비활성화 및 드래그 상태 초기화
+        /// </summary>
         public void Hide()
         {
             gameObject?.SetActive(false);
             ResetDraggedItem();
         }
 
+        /// <summary>
+        /// 아이템 설명 초기화 및 모든 아이템 선택 해제
+        /// </summary>
         public void ResetSelection()
         {
             _itemDescription.ResetDescription();
             DeselectAllItems();
         }
 
+        /// <summary>
+        /// 모든 아이템 UI 슬롯의 선택 해제 처리
+        /// </summary>
         private void DeselectAllItems()
         {
             foreach (UIInventoryItem item in listOfUIItems)
@@ -81,6 +108,12 @@ namespace Inventory.View
             }
         }
 
+        /// <summary>
+        /// 특정 인덱스의 아이템 UI 데이터를 갱신한다.
+        /// </summary>
+        /// <param name="itemIndex">아이템 인덱스</param>
+        /// <param name="itemImage">아이템 이미지</param>
+        /// <param name="itemQuantity">아이템 수량</param>
         public void UpdateData(int itemIndex, Sprite itemImage, int itemQuantity)
         {
             if (listOfUIItems.Count > itemIndex)
@@ -89,6 +122,13 @@ namespace Inventory.View
             }
         }
 
+        /// <summary>
+        /// 특정 인덱스 아이템 설명 UI 갱신
+        /// </summary>
+        /// <param name="itemIndex"></param>
+        /// <param name="itemImage"></param>
+        /// <param name="name"></param>
+        /// <param name="description"></param>
         public void UpdateDescription(int itemIndex, Sprite itemImage, string name, string description)
         {
             _itemDescription.SetDescription(itemImage, name, description);
@@ -97,7 +137,12 @@ namespace Inventory.View
         }
 
         #region Execute Method
-        public void HandleItemSelection(UIInventoryItem item)
+        /// <summary>
+        /// 아이템 슬롯 선택 시 호출
+        /// 선택한 아이템 인덱스를 찾아 아이템 설명 요청
+        /// </summary>
+        /// <param name="item"></param>
+        public void SelectItem(UIInventoryItem item)
         {
             int index = listOfUIItems.IndexOf(item);
             if (index == -1)
@@ -106,24 +151,31 @@ namespace Inventory.View
             OnDescriptionRequested?.Invoke(index);
         }
 
-        private void HandleBegingDrag(UIInventoryItem item)
+        /// <summary>
+        /// 아이템 드래그 시작 시 호출
+        /// 현재 드래그 중인 아이템 인덱스 저장 후,
+        /// 설명 요청하고, 
+        /// 드래그 중 이벤트 발생
+        /// </summary>
+        /// <param name="item"></param>
+        private void BegingDrag(UIInventoryItem item)
         {
             int index = listOfUIItems.IndexOf(item);
             if (index == -1)
                 return;
 
             curDraggedIndex = index;
-            HandleItemSelection(item);
+            SelectItem(item);
             OnStartDragging?.Invoke(index);
         }
 
-        public void CreateDraggedItem(Sprite sprite, int quantity)
-        {
-            _mouseFollower.Toggle(true);
-            _mouseFollower.SetData(sprite, quantity);
-        }
+        
 
-        private void HandleSwap(UIInventoryItem item)
+        /// <summary>
+        /// 아이템 드래그 앤 드롭을 통한 자리바꿈 이벤트 발생하는 메서드
+        /// </summary>
+        /// <param name="item"></param>
+        private void Swap(UIInventoryItem item)
         {
             int index = listOfUIItems.IndexOf(item);
             if (index == -1)
@@ -133,18 +185,41 @@ namespace Inventory.View
             OnSwapItems?.Invoke(curDraggedIndex, index);
         }
 
+
+        /// <summary>
+        /// MouseFollower 활성화 후,
+        /// 마우스 위치에 똑같이 복사
+        /// </summary>
+        /// <param name="sprite"></param>
+        /// <param name="quantity"></param>
+        public void CreateDraggedItem(Sprite sprite, int quantity)
+        {
+            _mouseFollower.Toggle(true);
+            _mouseFollower.SetData(sprite, quantity);
+        }
+
+
+        /// <summary>
+        /// MouseFollower 비활성 화 후,
+        /// 선택 중인 아이템 인덱스 -1로 리셋
+        /// </summary>
         private void ResetDraggedItem()
         {
             _mouseFollower.Toggle(false);
             curDraggedIndex = -1;
         }
 
-        private void HandleEndDrag(UIInventoryItem item)
+        private void EndDrag(UIInventoryItem item)
         {
             ResetDraggedItem();
         }
 
-        private void HandleShowItemActions(UIInventoryItem item)
+        /// <summary>
+        /// 아이템 우클릭 액션 요청 시 이벤트 발생
+        /// 우클릭 기능 삭제 예정
+        /// </summary>
+        /// <param name="item"></param>
+        private void ShowItemActions(UIInventoryItem item)
         {
             int index = listOfUIItems.IndexOf(item);
             if (index == -1)
@@ -153,8 +228,6 @@ namespace Inventory.View
             }
             OnItemActionRequested?.Invoke(index);
         }
-
-
         #endregion
     }
 }
