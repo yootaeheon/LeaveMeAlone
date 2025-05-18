fec869da0513cf534897968364ecd653ed766b00
RyooTaeHeon <taroni55@naver.com>
2025-05-18 21:43:49 +0900

    [Feat] 모든 스탯 자료형 Int -> Flaot 변경

--- Assets/Scripts/Inventory/Item/EquipItemSO/EquipItemSO.cs
+++ Assets/Scripts/Inventory/Item/EquipItemSO/EquipItemSO.cs
@@ -20,21 +20,24 @@ namespace Inventory.Model
         public EquipmentType EquipmentType { get { return _equipmetnType;  } set { _equipmetnType = value; } }
 
         [Header("Weapon")]
-        [SerializeField] int _attackPower;
-        public int AttackPower { get { return _attackPower; } set { _attackPower = value; } }
+        [SerializeField] float _attackPower;
+        public float AttackPower { get { return _attackPower; } set { _attackPower = value; } }
 
-        [SerializeField] int _attackSpeed;
-        public int AttackSpeed { get { return _attackSpeed; } set { _attackSpeed = value; } }
+        [SerializeField] float _attackSpeed;
+        public float AttackSpeed { get { return _attackSpeed; } set { _attackSpeed = value; } }
 
-        [SerializeField] int _criticalChance; // 크리티컬 확률
-        public int CriticalChance { get { return _criticalChance; } set { _criticalChance = value; } }
+        [SerializeField] float _criticalChance; // 크리티컬 확률
+        public float CriticalChance { get { return _criticalChance; } set { _criticalChance = value; } }
 
         [Header("Armor & Helmet & Back")]
-        [SerializeField] int _plusHp; // 추가 Hp
-        public int PlusHp { get { return _plusHp; } set { _plusHp = value; } }
+        [SerializeField] float _plusHp; // 추가 Hp
+        public float PlusHp { get { return _plusHp; } set { _plusHp = value; } }
 
-        [SerializeField] int _recoverHpPerSecond; // 초당 Hp 회복량
-        public int RecoverHpPerSecond { get { return _recoverHpPerSecond; } set { _recoverHpPerSecond = value; } }
+        [SerializeField] float _recoverHpPerSecond; // 초당 Hp 회복량
+        public float RecoverHpPerSecond { get { return _recoverHpPerSecond; } set { _recoverHpPerSecond = value; } }
+
+        [SerializeField] float _defensePower;
+        public float DefensePower { get { return _defensePower; } set { _defensePower = value; } }
 
 
     }
