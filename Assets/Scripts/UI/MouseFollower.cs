a2e645ae83af507a1bbe1ebb155f0934d748d13d
RyooTaeHeon <taroni55@naver.com>
2025-05-21 23:45:43 +0900

    [Feat] UI 사이즈 수정

--- Assets/Scripts/UI/MouseFollower.cs
+++ Assets/Scripts/UI/MouseFollower.cs
@@ -4,65 +4,51 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// 드래그 앤 드롭 시 아이템UI프리팹을 투명하게 마우스 위치로 띄워주고 그 UI에 드래그한 아이템을 똑같이 세팅해줌
+/// 드래그 앤 드롭 시 아이템 UI 프리팹을 마우스 위치에 띄워주고, 드래그한 아이템 정보를 복사함
 /// </summary>
 public class MouseFollower : MonoBehaviour
 {
-    [SerializeField] Canvas _canvas;
-
-    [SerializeField] UIInventoryItem _item;
+    [SerializeField] Canvas _canvas; // 연결된 캔버스
+    [SerializeField] UIInventoryItem _item; // 드래그 중 표시할 아이템 UI
 
     private void Awake()
     {
+        // 부모 오브젝트에서 캔버스를 찾아 연결
         _canvas = transform.root.GetComponent<Canvas>();
         _item = GetComponentInChildren<UIInventoryItem>();
 
-        // worldCamera가 null일 경우 Camera.main을 할당
-        if (_canvas.worldCamera == null) _canvas.worldCamera = Camera.main;
+        // Overlay 모드에선 worldCamera 필요 없음
     }
 
     /// <summary>
-    /// 드래그한 아이템을 마우스 위치에 똑같이 초기화;
+    /// 아이템 정보를 설정하여 UI에 표시
     /// </summary>
-    /// <param name="sprite"></param>
-    /// <param name="quantity"></param>
     public void SetData(Sprite sprite, int quantity)
     {
         _item.SetData(sprite, quantity);
     }
 
     /// <summary>
-    /// 마우스 포인터와 2D 상의 캔버스의 RectTransform 위치 변환을 수행
-    /// 모바일 환경과 에디터상에서 마우스의 위치가 상시 존재하지 않거나 할당되지 않는 위치로 갔을 때 에러가 발생하므로 조건 체크해줌.
+    /// Overlay 모드에서는 마우스 위치를 곧바로 위치로 설정 가능
     /// </summary>
     private void Update()
     {
-        if (_canvas.worldCamera == null) return;
-
         Vector2 screenPos = Input.mousePosition;
 
-        // 마우스 위치가 유효하지 않은 경우 early return
+        // 마우스 좌표가 유효하지 않은 경우 방어 코드
         if (float.IsInfinity(screenPos.x) || float.IsInfinity(screenPos.y) ||
             float.IsNaN(screenPos.x) || float.IsNaN(screenPos.y))
         {
             return;
         }
 
-        Vector2 pos;
-        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(
-                (RectTransform)_canvas.transform,
-                screenPos,
-                _canvas.worldCamera,
-                out pos))
-        {
-            transform.position = _canvas.transform.TransformPoint(pos);
-        }
+        // RectTransform.position은 월드 좌표지만 Overlay에선 스크린 좌표 그대로 사용 가능
+        transform.position = screenPos;
     }
 
     /// <summary>
-    /// Toggle이 True일 때만 마우스 위치에 투명한 프리팹이 보임
+    /// 마우스 따라다니는 UI 표시 토글
     /// </summary>
-    /// <param name="value"></param>
     public void Toggle(bool value)
     {
         Debug.Log($"Item Toggled {value}");
