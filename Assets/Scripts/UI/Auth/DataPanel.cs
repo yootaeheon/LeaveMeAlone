02302f2374fe4c179c2d4ec5bb07157623c15d02
RyooTaeHeon <taroni55@naver.com>
2025-06-04 00:31:15 +0900

    [Feat] 로그인 기능 구현

---
+++ Assets/Scripts/UI/Auth/DataPanel.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DataPanel : UIBinder
+{
+    private void Awake()
+    {
+        BindAll();
+    }
+    private void OnEnable()
+    {
+        DatabaseManager.Instance.onLevelChanged.AddListener(LevelChanged);
+        DatabaseManager.Instance.onLevelChanged.AddListener(CurStageChanged);
+        DatabaseManager.Instance.onLevelChanged.AddListener(GoldChanged);
+    }
+
+    private void Start()
+    {
+        GetUI<TextMeshProUGUI>("NickNameText").text = BackendManager.Auth.CurrentUser.DisplayName;
+    }
+    
+
+    private void LevelChanged()
+    {
+        GetUI<TextMeshProUGUI>("LevelText").text = $"{DatabaseManager.Instance.Level}.LV";
+    }
+
+    private void CurStageChanged()
+    {
+        GetUI<TextMeshProUGUI>("CurStage").text = $"{DatabaseManager.Instance.CurStage}.Stage";
+    }
+
+    private void GoldChanged()
+    {
+        GetUI<TextMeshProUGUI>("GoldText").text = $"{DatabaseManager.Instance.Gold}G";
+    }
+
+}
