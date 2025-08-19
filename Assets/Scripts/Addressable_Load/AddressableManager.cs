5b522cbad5a466d95b3b6ca7b9a708f6bdf57157
TaeHeon Ryoo <taroni55@naver.com>
2025-08-19 17:51:06 +0900

    [Feat] 어드레서블 매니저 테스트 코드 추가

--- Assets/Scripts/Addressable_Load/AddressableManager.cs
+++ Assets/Scripts/Addressable_Load/AddressableManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,6 +10,41 @@ using UnityEngine.UI;
 /// ReleaseAsset() <-> LoadAssetAsync
 public class AddressableManager : MonoBehaviour
 {
+    [SerializeField] GameObject _prefab;
+    [SerializeField] Material _material;
+    [SerializeField] AudioClip _audioClip;
+    [SerializeField] Dictionary<string, GameObject> _prefabDict = new Dictionary<string, GameObject>();
+
+    private void Test()
+    {
+        // prefab = Resources.Load<GameObject>("Prefabs/Player");
+        _prefab = Addressables.LoadAssetAsync<GameObject>("Playe").WaitForCompletion();
+        Addressables.LoadAssetAsync<GameObject>("Playe").Completed += OnCompleted;
+    }
+
+
+    // 기존: private Action OnCompleted()
+    // 수정: Completed 이벤트 핸들러는 Action<AsyncOperationHandle<GameObject>> 시그니처여야 함
+    private void OnCompleted(UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<GameObject> handle)
+    {
+        if (handle.Status == UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationStatus.Succeeded)
+        {
+            // 성공적으로 로드됨
+            GameObject loadedObj = handle.Result;
+            // 필요한 처리 작성
+        }
+        else
+        {
+            // 로드 실패 처리
+            Debug.LogError("Addressable 로드 실패: " + handle.OperationException);
+        }
+    }
+
+    //=====================================================================================================================
+    // 위는 테스트용 코드
+    //=====================================================================================================================
+
+
     [SerializeField] AssetReferenceGameObject[] _gameObjs;
 
    /* private AssetReferenceT<AudioClip> SoundBGM;
